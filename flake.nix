207edd08dbe7646124c8e95f4d5ecda85ab83898
Maaz Khokhar <khokharmaaz@gmail.com>
2026-07-04 11:02:52 -0500

    removed devenv

--- flake.nix
+++ flake.nix
@@ -1,37 +1,46 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+
+          packages = with pkgs; [
+            # flyctl
+          ];
+
+          EIPI_LISTEN = "0.0.0.0:2222";
+          EIPI_NO_LIMIT = "1";
+
+          shellHook = ''
+            echo "------eipi.boo--------"
+          '';
+
+          # env = {
+          #   # Linux
+          #   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          #     pkgs.stdenv.cc.cc.lib
+          #   ];
+          #   # MacOS
+          #   DYLD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          #     pkgs.stdenv.cc.cc.lib
+          #   ];
+          # };
+        };
+      });
     };
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs = {
-    nixpkgs,
-    devenv,
-    ...
-  } @ inputs: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = function: nixpkgs.lib.genAttrs systems (system: function (import nixpkgs {inherit system;}));
-  in {
-    devShells = forAllSystems (pkgs: {
-      default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-
-        modules = [./devenv.nix];
-      };
-    });
-  };
 }
